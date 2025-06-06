3adc3ce9ff4ac6a7001b7c20baa32806396f1d64
dsilvag <dsilvag@users.noreply.github.com>
2025-06-06 10:51:12 +0200

    Disable pagination all

--- matricules-gestio/app/Providers/AppServiceProvider.php
+++ matricules-gestio/app/Providers/AppServiceProvider.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Event;
 use SocialiteProviders\Manager\SocialiteWasCalled;
 use Filament\Support\Assets\Css;
 use Filament\Support\Facades\FilamentAsset;
+use Filament\Tables\Table;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -30,6 +31,10 @@ class AppServiceProvider extends ServiceProvider
         FilamentAsset::register([
             Css::make('custom-stylesheet', __DIR__ . '/../../resources/css/filament.css'),
         ]);
+        //Treure pagination tots
+        Table::configureUsing(function (Table $table) {
+            $table->paginated([10, 25, 50, 100]);
+        });
 
     }
 }
